565f80cd6efab14e9d916f23a62f56374003ad56
Sh0k0 <vlad6661866@gmail.com>
2021-10-01 01:08:06 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,29 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
-/**
- * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
- * @return {String | Boolean} name of the team or false
- * in case of incorrect members
- *
- * @example
- * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+// /**
+//  * Create name of dream team based on the names of its members
+//  *
+//  * @param {Array} members names of the members
+//  * @return {String | Boolean} name of the team or false
+//  * in case of incorrect members
+//  *
+//  * @example
+//  *
+//  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
+//  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
+//  *
+//  */
+
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  if (members.length === 0) return false;
+
+  return members
+    .filter((a) => typeof a === "string" || a instanceof String)
+    .map((a) => a.trim()[0].toUpperCase())
+    .sort()
+    .join("");
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }

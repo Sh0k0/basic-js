e1c64b7487797a338fdfea9e88b247844e51ed53
Sh0k0 <vlad6661866@gmail.com>
2021-10-01 00:59:01 +0300

    feat: add count-cats solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,11 +1,24 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from '../extensions/index.js'
 
-export default function countCats(matrix) {
-  let n = 0;
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] === '^^') {
-        n += 1;
-      }
+/**
+ * Given matrix where you have to find cats by ears "^^"
+ *
+ * @param {Array<Array>} matrix
+ * @return {Number} count of cats found
+ *
+ * @example
+	@@ -14,7 +14,14 @@ import { NotImplementedError } from '../extensions/index.js';
+ * ]) => 3`
+ *
+ */
+export default function countCats(backyard) {
+  let cats = 0
+  for (let i = 0; i < backyard.length; i++) {
+    for (let j = 0; j <= backyard[i].length; j++) {
+      const box = backyard[i][j]
+      box === '^^' ? cats++ : ''
     }
-  }  return n;
+  }
+
+  return cats
+}
